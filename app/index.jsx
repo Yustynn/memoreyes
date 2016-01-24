cd5939b32e8115e4c71c38a22c28e4832dd43beb
Yustynn <yustynn@gmail.com>
2016-01-24 13:52:52 -0500

    Get react-router working

--- app/index.jsx
+++ app/index.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
+import {Router, Route, Link} from 'react-router';
 
 import Frame from './components/Frame';
 import Main from './components/Main'
 import Home from './components/home/Home'
+import Matching from './components/matching/Matching';
 
-ReactDOM.render(
-    <Frame>
-        <Main>
-            <Home>
-            </Home>
-        </Main>
-    </Frame>,
-    document.getElementById('app')
-);
+const App = React.createClass({
+    render: function() {
+        return <Frame>
+            <Main>
+                <Link to='/home'>Home</Link>
+                <Link to='/matching'>Matching</Link>
+                { this.props.children }
+            </Main>
+        </Frame>
+    }
+})
+
+render((
+    <Router>
+        <Route path='/' component={ App }>
+            <Route path='/home' component={ Home } />
+            <Route path='/matching' component={ Matching }/>
+        </Route>
+    </Router>
+), document.getElementById('app'));
